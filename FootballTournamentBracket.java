29041565f563b4468768582449ccc9f576c5b9de
Hiram K <hiram.kamau@outlook.com>
2023-03-23 06:01:53 +0300

    Update FootballTournamentBracket.java

--- FootballTournamentBracket.java
+++ FootballTournamentBracket.java
@@ -1,34 +1,122 @@
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class FootballTournamentBracket {
 
-    public static String[] generateBracket(int numTeams) {
-        if (numTeams < 2 || numTeams % 2 != 0) {
-            return null;
-        }
-        int numRounds = (int) (Math.log(numTeams) / Math.log(2));
-        int numMatches = numTeams / 2;
-        String[] bracket = new String[numMatches * numRounds];
-        int matchIndex = 0;
-        for (int round = 1; round <= numRounds; round++) {
-            int numMatchesThisRound = numMatches / (int) Math.pow(2, round - 1);
-            for (int match = 1; match <= numMatchesThisRound; match++) {
-                int team1 = (match - 1) * (int) Math.pow(2, round - 1) + 1;
-                int team2 = team1 + (int) Math.pow(2, round - 1);
-                bracket[matchIndex++] = team1 + " vs. " + team2;
-            }
-        }
-        return bracket;
+    private final BracketNode root;
+    private int numTeams;
+
+    public FootballTournamentBracket(int numTeams) {
+        this.numTeams = numTeams;
+        this.root = createBracket(numTeams);
     }
 
-    public static void main(String[] args) {
-        int numTeams = 16;
-        String[] bracket = generateBracket(numTeams);
-        if (bracket == null) {
-            System.out.println("Invalid number of teams.");
+    public void setTeamName(int index, String name) {
+        BracketNode node = getNode(index);
+        if (node != null) {
+            node.teamName = name;
+        }
+    }
+
+    public void playMatch(int index) {
+        BracketNode node = getNode(index);
+        if (node != null && node.left != null && node.right != null) {
+            String winner = simulateMatch(node.left.teamName, node.right.teamName);
+            node.teamName = winner;
+        }
+    }
+
+    public String getWinner() {
+        return root.teamName;
+    }
+
+    private BracketNode getNode(int index) {
+        if (index < 0 || index >= numTeams) {
+            return null;
+        }
+
+        BracketNode node = root;
+        int level = (int) (Math.log(numTeams) / Math.log(2));
+
+        while (level > 0) {
+            if (index < numTeams / 2) {
+                node = node.left;
+            } else {
+                node = node.right;
+                index -= numTeams / 2;
+            }
+            numTeams /= 2;
+            level--;
+        }
+
+        return node;
+    }
+
+    private String simulateMatch(String team1, String team2) {
+        Random rand = new Random();
+        int score1 = rand.nextInt(5);
+        int score2 = rand.nextInt(5);
+
+        System.out.printf("%s %d - %d %s%n", team1, score1, score2, team2);
+
+        if (score1 > score2) {
+            return team1;
+        } else if (score2 > score1) {
+            return team2;
         } else {
-            for (int i = 0; i < bracket.length; i++) {
-                System.out.println("Match " + (i + 1) + ": " + bracket[i]);
+            // in case of a tie, randomly choose a winner
+            if (rand.nextBoolean()) {
+                return team1;
+            } else {
+                return team2;
             }
         }
     }
+
+    public static void assignTeamNames(BracketNode node, Scanner scanner) {
+        if (node == null) {
+            return;
+        }
+
+        if (node.left == null && node.right == null) {
+            System.out.print("Enter the name of team " + node.teamName + ": ");
+            node.teamName = scanner.nextLine();
+            return;
+        }
+
+        assignTeamNames(node.left, scanner);
+        assignTeamNames(node.right, scanner);
+    }
+
+    public class BracketNode {
+
+        public BracketNode left;
+        public BracketNode right;
+        public String teamName;
+
+        public BracketNode(String teamName) {
+            this.teamName = teamName;
+        }
+    }
+
+    public BracketNode createBracket(int numTeams) {
+        BracketNode root = new BracketNode(null);
+        createSubtree(root, numTeams);
+        return root;
+    }
+
+    private void createSubtree(BracketNode node, int numTeams) {
+        if (numTeams == 1) {
+            node.teamName = "Team " + numTeams;
+            return;
+        }
+
+        node.left = new BracketNode(null);
+        node.right = new BracketNode(null);
+
+        createSubtree(node.left, numTeams / 2);
+        createSubtree(node.right, numTeams / 2);
+    }
+
 }
